f6b3224710572f1ca209b73d047bac6eb4c1527f
Dokitsu <projectfuzzle@gmail.com>
2018-10-07 03:22:23 +0100

    player
    can pick up key and put in on the standard in the middle of the room

---
+++ Emergent_Prototype/Assets/_Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform look;
+    public GameObject key;
+
+    public Pillar pills;
+
+    public bool haskey;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        haskey = false;
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            float Distance = 2.5f;
+
+            Ray ray = new Ray(look.position, look.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Distance))
+            {
+                Debug.Log(hit.transform.gameObject);
+                if (hit.transform.gameObject.name == "keey" )
+                {
+                    Destroy(hit.transform.gameObject);
+                    key.SetActive(true);
+                    haskey = true;
+                }
+
+                if (hit.transform.gameObject.name == "Pillar")
+                {
+                    if(haskey == true)
+                    {
+                        pills = hit.transform.gameObject.GetComponent<Pillar>();
+                        key.SetActive(false);
+                        haskey = false;
+                        pills.Activekey();
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+
+            }
+
+
+        }
+
+    }
+}
