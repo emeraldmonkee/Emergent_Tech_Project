e373d159e865fbfcf1ee9531d166ee2500c11218
Matthew Stageman <mattstageman@gmail.com>
2018-10-10 11:42:11 +0100

    Generic Countdown Timer
    Created a canvas with a generic countdowntimer script attached to a text object on the relevant canvas.

---
+++ Emergent_Prototype/Assets/_Scripts/Countdown_Clock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown_Clock : MonoBehaviour
+{
+    private Text timerText;
+
+    private float timeLeft;
+    string mins;
+    string seconds;
+
+	void Start ()
+    {
+        timerText = GetComponent<Text>();
+        timeLeft = 120;
+	}
+	
+
+	void Update ()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            mins = Mathf.Floor(((int)timeLeft % 3600) / 60).ToString("00");
+            seconds = ((int)timeLeft % 60).ToString("00");
+            timerText.text = mins + " : " + seconds;
+        }
+	}
+}
