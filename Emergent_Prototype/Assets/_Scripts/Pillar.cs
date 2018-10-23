241c9fa90f36e1ee74111b7c8e5667b5cdc6055d
Dokitsu <projectfuzzle@gmail.com>
2018-10-23 20:38:54 +0100

    Changes

--- Emergent_Prototype/Assets/_Scripts/Pillar.cs
+++ Emergent_Prototype/Assets/_Scripts/Pillar.cs
@@ -6,14 +6,14 @@ public class Pillar : MonoBehaviour {
 
     public GameObject key;
 
-    public GameObject roomnode;
+    //public GameObject roomnode;
 
-    public GameObject doors;
+    //public GameObject doors;
     public GameObject[] slide;
 
     public SlidingDoor open;
 
-    public Transform[] points;
+    //public Transform[] points;
 
 	// Use this for initialization
 	void Start () {
@@ -30,13 +30,15 @@ public class Pillar : MonoBehaviour {
     //Instantiates new rooms at each of the exits and opens the doors 'SlidingDoor.cs' -Tom
     public void Activekey()
     {
-        key.SetActive(true);
-        doors.SetActive(false);
+        Debug.Log("opneinihnrihgnrehi");
 
-        for (int i = 0; i < points.Length; i++)
-        {
-            Instantiate(roomnode, points[i].transform.position, points[i].transform.rotation);
-        }
+        key.SetActive(true);
+        //doors.SetActive(false);
+
+        //for (int i = 0; i < points.Length; i++)
+        //{
+        //    Instantiate(roomnode, points[i].transform.position, points[i].transform.rotation);
+        //}
 
         for (int i = 0; i < slide.Length; i++)
         {
---
+++ Emergent_Prototype/Assets/_Scripts/Rooms.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rooms {
+
+    public Vector3 gridpos;
+
+    public int type;
+
+    public bool doorTop, doorBot, doorLeft, doorRight;
+
+    public Rooms(Vector3 _gridpos, int _type)
+    {
+        gridpos = _gridpos;
+        type = _type;
+    }
+}
