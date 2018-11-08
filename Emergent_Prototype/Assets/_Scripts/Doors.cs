a3e0d0436145c3ea752b7680ef0a709ddd500154
Dokitsu <projectfuzzle@gmail.com>
2018-11-08 11:48:06 +0000

    corridors

---
+++ Emergent_Prototype/Assets/_Scripts/Doors.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour{
+
+    private bool triggered = false;
+    public GameObject connection;
+    public GameObject Wall;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 4))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            if (triggered == false)
+            {
+                triggered = true;
+
+            }
+
+        }
+        else
+        {
+            triggered = true;
+            Wall.SetActive(true);
+            Destroy(connection);
+        }
+
+    }
+
+
+
+}
