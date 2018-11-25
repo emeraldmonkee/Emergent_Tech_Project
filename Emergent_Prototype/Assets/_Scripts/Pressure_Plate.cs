6d6e31e0f33b6b7efa1881f45d050a3de1e6b700
Matthew Stageman <mattstageman@gmail.com>
2018-11-25 12:01:49 +0000

    Added a new podem when the button is pressed (via the leap motion) the challenge will spawn

--- Emergent_Prototype/Assets/_Scripts/Pressure_Plate.cs
+++ Emergent_Prototype/Assets/_Scripts/Pressure_Plate.cs
@@ -8,12 +8,26 @@ public class Pressure_Plate : MonoBehaviour
 
     public GameObject challengeSpawner;
 
-    private void OnTriggerEnter(Collider other)
+    public GameObject podem;
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == ("Player"))
+    //    {
+    //        Clock.GetComponent<Countdown_Clock>().gameIsPlaying = true;
+    //        this.transform.position -= new Vector3(0, 5, 0);
+    //        Clock.GetComponent<Countdown_Clock>().timeLeft = 60;
+    //        challengeSpawner.GetComponent<Challenge_Spawner>().SpawnChallenge();
+    //    }
+    //}
+
+    private void Update()
     {
-        if (other.tag == ("Player"))
+        if (this.transform.localPosition.y <= 1.07)
         {
             Clock.GetComponent<Countdown_Clock>().gameIsPlaying = true;
-            this.transform.position -= new Vector3(0, 5, 0);
+            podem.transform.position -= new Vector3(0f, 10f, 0f);
+            this.transform.localPosition += new Vector3(0f, 0.07f, 0f);
             Clock.GetComponent<Countdown_Clock>().timeLeft = 60;
             challengeSpawner.GetComponent<Challenge_Spawner>().SpawnChallenge();
         }
