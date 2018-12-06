ccea396a65e2df3dee90dd30bd0bf78a343ca917
Dokitsu <projectfuzzle@gmail.com>
2018-12-06 20:09:48 +0000

    uni

---
+++ Emergent_Prototype/Assets/_Scripts/Podium_Challenge/Podium_Challenge_Manager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Podium_Challenge_Manager : MonoBehaviour
+{
+    public GameObject Keey;
+    public GameObject Podium;
+
+    private int Podium_Cube;
+
+    void Start()
+    {
+        Podium_Cube = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Podium_Cube")
+        {
+            Podium_Cube++;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if(Podium_Cube >= 8)
+        {
+            Keey.SetActive(true);
+            Podium.SetActive(true);
+        }
+    }
+}
